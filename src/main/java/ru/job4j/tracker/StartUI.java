c114946d0eda14376c452c4c5eaac34a04b115bf
summeray <igor.budnitski@gmail.com>
2021-01-25 06:15:31 +0200

    6. Date. Отображение даты. [#367060]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,10 +5,8 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
-        System.out.println("Текущие дата и время до форматирования: " + item.getCreated());
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String currentDateTimeFormat = item.getCreated().format(formatter);
-        System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
-
+        System.out.println(currentDateTimeFormat);
     }
 }
