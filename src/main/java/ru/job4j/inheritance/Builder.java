0d1ba991c0e001a93a72e104c377fcc21a2b6ce8
summeray <igor.budnitski@gmail.com>
2021-01-27 05:47:42 +0200

    3. Реализация профессий в коде

---
+++ src/main/java/ru/job4j/inheritance/Builder.java
@@ -0,0 +1,9 @@
+package ru.job4j.inheritance;
+
+public class Builder extends Engineer{
+    private String speciality;
+
+    public void understandBlueprints(){
+
+    }
+}
---
+++ src/main/java/ru/job4j/inheritance/Programmer.java
@@ -0,0 +1,10 @@
+package ru.job4j.inheritance;
+
+public class Programmer extends Engineer {
+    private String programmingLanguage;
+
+    public void writeCode() {
+
+    }
+
+}
